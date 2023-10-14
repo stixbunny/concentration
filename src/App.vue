55fbd9babb21d402d983f3199c0fa500ac57584a
stixbunny <milenko.castillo@gmail.com>
2023-10-14 13:31:51 -0300

    feature: transition animation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <script setup>
 import TheGame from './components/TheGame.vue';
 import NameRegister from './components/NameRegister.vue';
+import { useStorage } from '@vueuse/core';
+import ModalTransition from './components/ModalTransition.vue';
+
+const user = useStorage('user-name', '', localStorage);
+
 </script>
 
 <template>
-  <NameRegister />
+  <ModalTransition>
+    <NameRegister v-if="!user" />
+  </ModalTransition>
   <Suspense>
     <TheGame />
   </Suspense>
 </template>
 
-<style scoped></style>
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
