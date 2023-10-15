8f86ce953bbd5b293414b8366169e1b94f3cf309
stixbunny <milenko.castillo@gmail.com>
2023-10-14 21:50:55 -0300

    leaving animation and name saving logic

--- src/components/NameRegister.vue
+++ src/components/NameRegister.vue
@@ -1,11 +1,19 @@
 <script setup>
+import { ref } from 'vue';
 import { useStorage } from '@vueuse/core';
 import ModalTransition from './ModalTransition.vue';
 
 const user = useStorage('user-name', '', localStorage);
 
+const error = ref(false);
+
 function checkName(event) {
-  console.log(event.target.elements.name.value);
+  const name = event.target.elements.name.value.trim();
+  if( name.length < 3 || name.length > 10) {
+    error.value = true;
+  } else {
+    user.value = name;
+  }
 }
 </script>
 
@@ -27,8 +35,8 @@ function checkName(event) {
           placeholder="Jugador"
           class="name-register_name rounded shadow bg-white border p-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         />
-        <p class="name-register_warning text-red-500 text-xs italic hidden">
-          Por favor ingresa un nombre de 3 cacaracteres mínimo.
+        <p class="name-register_warning text-red-500 text-xs italic data-[error=false]:hidden" :data-error="error">
+          Ingresa un nombre entre 3 y 10 caracteres.
         </p>
         <button
           type="submit"
